f2fdcd791cb671c3130feebd23d54a933102e233
AsharuDheen <AsharuAshkar@gmail.com>
2020-08-18 21:33:41 +0530

    Updates

---
+++ precache-manifest.816611ced4d6bbb1c685d2489b1dc430.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ae41e5ebc8826448a1e7115d5455f3a",
+    "url": "/todo-app2/index.html"
+  },
+  {
+    "revision": "ee246c1cd8264c346625",
+    "url": "/todo-app2/static/css/main.727301b2.chunk.css"
+  },
+  {
+    "revision": "85e6e348f250be2e8c44",
+    "url": "/todo-app2/static/js/2.2984d714.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app2/static/js/2.2984d714.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee246c1cd8264c346625",
+    "url": "/todo-app2/static/js/main.8cb5541e.chunk.js"
+  },
+  {
+    "revision": "7637ff9083bc55543834",
+    "url": "/todo-app2/static/js/runtime-main.7a7a801f.js"
+  }
+]);
